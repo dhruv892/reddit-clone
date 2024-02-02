2e5c1f718e7546e1d9f3a41840aed8ef8581652f
Darshan Vaishya <DarshanVaishya3@gmail.com>
2024-02-02 17:32:39 -0500

    Added description to post comp + removed default items

--- client/src/components/post.jsx
+++ client/src/components/post.jsx
@@ -6,9 +6,12 @@ export default function Post() {
 	const [score, setScore] = useState(0);
 	const post = {
 		url: "https://github.com/DarshanVaishya/reddit-exercise/blob/master/src/components/styles/Post.css",
-		title: "Hello world",
+		title:
+			"[Darren Dreger] Trade call between the Jets and Canadiens happening soon. Sean Monahan to the Jets for a 1st and a conditional pick. ",
+		description:
+			"So, a very close relative of mine is a wedding planner and has spilled some tea on celebs and their demands before agreeing to perform at weddings. I 100% believe him because he’s least interested in spreading Bollywood “gossip” but still, believe at your own risk.  RS comes with a huge list of demands, including but not limited to, private jet ofc. Some mandatory white powder and female company. Specific brands of liquor, a number of bodyguards. He once also denied to come for a wedding AFTER getting his payment for which he was “reprimanded” by a certain Dubai Sheikh and the money was returned.  Vishal Shekhar do a set of about 2-3 hours out of which they hardly perform for 30 minutes together. They take turns and Shekhar is the one who performs better and longer. They are just milking their brand name but apparently do not get along.  Badshah brings along fellow singers who sing for him. You may as well put on a Badshah playlist instead of calling him and paying him a bomb.  There are more stories which I can share based on the response to this post (i.e. how much trouble i get into or not lol) Anyone else has any such wedding tea to spill?",
 		author: "Dhruv Barot",
-		time: 1706905390338,
+		time: 1648166400000,
 		num_comments: 5,
 	};
 
@@ -25,13 +28,17 @@ export default function Post() {
 			</div>
 
 			<div className="post-details">
-				<div className="wrapper">
+				<div className="post-wrapper">
 					<a href={post.url} className="post-link">
 						<span className="post-title">{post.title}</span>
 					</a>
 				</div>
 
-				<div className="wrapper">
+				<div className="post-wrapper">
+					<span className="post-description">{post.description}</span>
+				</div>
+
+				<div className="post-wrapper">
 					Submitted {moment(post.time).fromNow()} by {post.author}
 				</div>
 
