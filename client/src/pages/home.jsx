75bad545d246e2c61157e2bda42fa9fa8ca0a72c
Dhruv Barot <dhruv892@dhruvs-mbp.home>
2024-02-19 18:07:48 -0500

    fixed logout

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -7,61 +7,64 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export function Home() {
-	const refreshPostsAtom = useRecoilRefresher_UNSTABLE(fetchPost);
-	const refresh = useRecoilValue(refreshPosts);
-	const navigate = useNavigate();
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const refreshPostsAtom = useRecoilRefresher_UNSTABLE(fetchPost);
+    const refresh = useRecoilValue(refreshPosts);
+    const navigate = useNavigate();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-	const fetchSessionData = async () => {
-		try {
-			const response = await axios.get(
-				"http://localhost:3000/api/user/session",
-				{
-					withCredentials: true,
-				}
-			);
-			console.log(response);
-			if (response.status === 200) {
-				setIsLoggedIn(true);
-				// Now you can redirect the user or perform other actions
-			}
-		} catch (error) {
-			console.log("User is not authenticated");
-		}
-	};
+    const fetchSessionData = async () => {
+        try {
+            const response = await axios.get(
+                "http://localhost:3000/api/user/session",
+                {
+                    withCredentials: true,
+                }
+            );
+            console.log(response);
+            if (response.status === 200) {
+                setIsLoggedIn(true);
+                // Now you can redirect the user or perform other actions
+            }
+        } catch (error) {
+            console.log("User is not authenticated");
+        }
+    };
 
-	const logoutHandler = async () => {
-		try {
-			const response = await axios.get(
-				"http://localhost:3000/api/user/signout"
-			);
-			console.log(response);
-			setIsLoggedIn(false);
-		} catch (error) {
-			console.error(error);
-		}
-	};
+    const logoutHandler = async () => {
+        try {
+            const response = await axios.get(
+                "http://localhost:3000/api/user/signout",
+                {
+                    withCredentials: true,
+                }
+            );
+            console.log(response);
+            setIsLoggedIn(false);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-	useEffect(() => {
-		refreshPostsAtom();
-		fetchSessionData();
-	}, [refresh, refreshPostsAtom]);
+    useEffect(() => {
+        refreshPostsAtom();
+        fetchSessionData();
+    }, [refresh, refreshPostsAtom, isLoggedIn]);
 
-	return (
-		<>
-			{!isLoggedIn ? (
-				<button
-					onClick={() => {
-						navigate("/signUpIn");
-					}}
-				>
-					SignUpIn
-				</button>
-			) : (
-				<button onClick={() => logoutHandler()}>Log out</button>
-			)}
-			<CreatePost />
-			<Posts />
-		</>
-	);
+    return (
+        <>
+            {!isLoggedIn ? (
+                <button
+                    onClick={() => {
+                        navigate("/signUpIn");
+                    }}
+                >
+                    SignUpIn
+                </button>
+            ) : (
+                <button onClick={() => logoutHandler()}>Log out</button>
+            )}
+            <CreatePost />
+            <Posts />
+        </>
+    );
 }
