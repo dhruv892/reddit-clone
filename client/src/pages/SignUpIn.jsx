920e4a7abcd895776959a06e3af865d0ff794f3c
Dhruv Barot <dhruv892@dhruvs-mbp.home>
2024-02-18 19:55:33 -0500

    fixed signUpIn

--- client/src/pages/SignUpIn.jsx
+++ client/src/pages/SignUpIn.jsx
@@ -1,35 +1,100 @@
-export function SignUpIn() {
-	return (
-		<>
-			<div>
-				<form>
-					<h1>Sign Up</h1>
-					<div>
-						<label>Username</label>
-						<input type="text" placeholder="Username" />
-					</div>
-					<div>
-						<label>Password</label>
-						<input type="password" placeholder="Username" />
-					</div>
-					<input type="submit" value="Sign up" />
-				</form>
-			</div>
+import { useState } from "react";
 
-			<div>
-				<form>
-					<h1>Sign In</h1>
-					<div>
-						<label>Username</label>
-						<input type="text" placeholder="Username" />
-					</div>
-					<div>
-						<label>Password</label>
-						<input type="password" placeholder="Username" />
-					</div>
-					<input type="submit" value="Sign in" />
-				</form>
-			</div>
-		</>
-	);
+export function SignUpIn() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [usernameUp, setUsernameUp] = useState("");
+    const [passwordUp, setPasswordUp] = useState("");
+    const [usernameIn, setUsernameIn] = useState("");
+    const [passwordIn, setPasswordIn] = useState("");
+
+    const signUpHandler = (e) => {
+        e.preventDefault();
+
+        console.log(firstName, lastName, usernameUp, passwordUp);
+    };
+
+    const signInHandler = (e) => {
+        e.preventDefault();
+
+        console.log(usernameIn, passwordIn);
+    };
+
+    return (
+        <>
+            <div>
+                <form onSubmit={signUpHandler}>
+                    <h1>Sign Up</h1>
+                    <div>
+                        <label>First Name</label>
+                        <input
+                            type="text"
+                            name="firstName"
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                            placeholder="First Name"
+                        />
+                    </div>
+                    <div>
+                        <label>Last Name</label>
+                        <input
+                            type="text"
+                            name="lastName"
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                            placeholder="Last Name"
+                        />
+                    </div>
+                    <div>
+                        <label>Username</label>
+                        <input
+                            type="text"
+                            name="usernameUp"
+                            value={usernameUp}
+                            onChange={(e) => setUsernameUp(e.target.value)}
+                            placeholder="Username"
+                        />
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            name="passwordUp"
+                            value={passwordUp}
+                            onChange={(e) => setPasswordUp(e.target.value)}
+                            placeholder="Password"
+                        />
+                    </div>
+                    <input type="submit" value="Sign up" />
+                </form>
+            </div>
+
+            <div>
+                <form onSubmit={signInHandler}>
+                    <h1>Sign In</h1>
+                    <div>
+                        <label>Username</label>
+                        <input
+                            type="text"
+                            name="usernameIn"
+                            value={usernameIn}
+                            onChange={(e) => setUsernameIn(e.target.value)}
+                            placeholder="Username"
+                        />
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            name="passwordIn"
+                            value={passwordIn}
+                            onChange={(e) => setPasswordIn(e.target.value)}
+                            placeholder="Password"
+                        />
+                    </div>
+                    <input type="submit" value="Sign in" />
+                </form>
+            </div>
+        </>
+    );
 }
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -7,20 +7,33 @@ import {
 } from "recoil";
 import { postAtom, refreshPosts, fetchPost } from "../store/atoms";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 // import { useState } from "react";
 export function Home() {
     const postsLoadable = useRecoilValueLoadable(postAtom);
     const refreshPostsAtom = useRecoilRefresher_UNSTABLE(fetchPost);
     const refresh = useRecoilValue(refreshPosts);
+    const navigate = useNavigate();
 
     useEffect(() => {
         refreshPostsAtom();
     }, [refresh, refreshPostsAtom]);
 
+    // const SignUpInClickHandler = () => {
+    //     navigate("/signupin");
+    // };
+
     switch (postsLoadable.state) {
         case "hasValue":
             return (
                 <>
+                    <button
+                        onClick={() => {
+                            navigate("/signUpIn");
+                        }}
+                    >
+                        SignUpIn
+                    </button>
                     <CreatePost />
                     {postsLoadable.contents.map((post) => (
                         <Post key={post._id} post={post} />
