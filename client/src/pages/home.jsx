cc18788e6c2155c97474e99c59e0baed8107c4b1
Dhruv Barot <dhruv892@dhruvs-mbp.home>
2024-02-18 20:53:08 -0500

    fixed Posts to prevent prop drilling

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,16 +1,11 @@
-import Post from "../components/post";
+import Posts from "../components/Posts";
 import { CreatePost } from "../components/CreatePost";
-import {
-    useRecoilValue,
-    useRecoilValueLoadable,
-    useRecoilRefresher_UNSTABLE,
-} from "recoil";
-import { postAtom, refreshPosts, fetchPost } from "../store/atoms";
+import { useRecoilValue, useRecoilRefresher_UNSTABLE } from "recoil";
+import { refreshPosts, fetchPost } from "../store/atoms";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-// import { useState } from "react";
+
 export function Home() {
-    const postsLoadable = useRecoilValueLoadable(postAtom);
     const refreshPostsAtom = useRecoilRefresher_UNSTABLE(fetchPost);
     const refresh = useRecoilValue(refreshPosts);
     const navigate = useNavigate();
@@ -19,32 +14,17 @@ export function Home() {
         refreshPostsAtom();
     }, [refresh, refreshPostsAtom]);
 
-    // const SignUpInClickHandler = () => {
-    //     navigate("/signupin");
-    // };
-
-    switch (postsLoadable.state) {
-        case "hasValue":
-            return (
-                <>
-                    <button
-                        onClick={() => {
-                            navigate("/signUpIn");
-                        }}
-                    >
-                        SignUpIn
-                    </button>
-                    <CreatePost />
-                    {postsLoadable.contents.map((post) => (
-                        <Post key={post._id} post={post} />
-                    ))}
-                </>
-            );
-        case "loading":
-            return <div>Loading...</div>;
-        case "hasError":
-            return <div>Error: {postsLoadable.contents.message}</div>;
-        default:
-            return null;
-    }
+    return (
+        <>
+            <button
+                onClick={() => {
+                    navigate("/signUpIn");
+                }}
+            >
+                SignUpIn
+            </button>
+            <CreatePost />
+            <Posts />
+        </>
+    );
 }
