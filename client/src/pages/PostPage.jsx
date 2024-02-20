4e63122016d27abdb978f984d4d39ddf7ac50e19
Darshan Vaishya <DarshanVaishya3@gmail.com>
2024-02-19 19:56:42 -0500

    Added comments to post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router";
 import { postAtom } from "../store/atoms";
 import { useRecoilValueLoadable } from "recoil";
 import moment from "moment";
+import "./PostPage.css";
 
 export function PostPage() {
 	const postLoadable = useRecoilValueLoadable(postAtom);
@@ -18,19 +19,33 @@ export function PostPage() {
 			throw postLoadable.contents;
 	}
 
+	console.log(post.comments);
+
 	return (
 		<div>
-			<p>{post.title}</p>
+			<p className="post-title">{post.title}</p>
 			<pre>
-				<p>{post.content}</p>
+				<p className="post-content">{post.content}</p>
 			</pre>
 			<p>
-				Submitted {moment(parseInt(post.createdAt)).fromNow()} by {post.author}
+				Post submitted {moment(parseInt(post.createdAt)).fromNow()} by{" "}
+				{post.author}
 			</p>
 
 			<div>
 				<p>Comments</p>
-				{post.comments.map((comment) => comment)}
+				{post.comments.map((comment) => (
+					<div key={comment._id} className="post-comment">
+						<p>
+							{comment.author}{" "}
+							<span className="post-comment-time">
+								{moment(parseInt(comment.createdAt)).fromNow()}{" "}
+							</span>
+						</p>
+						<p>{comment.content}</p>
+						<span></span>
+					</div>
+				))}
 			</div>
 		</div>
 	);
