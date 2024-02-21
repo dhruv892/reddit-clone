db8ffb7daeabcc3df5886130aee19a3123cf79f1
Dhruv Barot <dhruv892@dhruvs-mbp.home>
2024-02-20 23:24:03 -0500

    changed axios defaults

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -8,68 +8,75 @@ import { PostComments } from "../components/PostComments";
 import axios from "axios";
 
 export function PostPage() {
-	const postLoadable = useRecoilValueLoadable(postAtom);
-	const params = useParams();
+    const postLoadable = useRecoilValueLoadable(postAtom);
+    const params = useParams();
+    axios.defaults.withCredentials = true;
 
-	let post;
-	switch (postLoadable.state) {
-		case "hasValue":
-			post = postLoadable.contents.find((p) => p._id === params.id);
-			break;
-		case "loading":
-			return <h1>Loading...</h1>;
-		case "hasError":
-			throw postLoadable.contents;
-	}
-	console.log(post);
+    let post;
+    switch (postLoadable.state) {
+        case "hasValue":
+            post = postLoadable.contents.find((p) => p._id === params.id);
+            break;
+        case "loading":
+            return <h1>Loading...</h1>;
+        case "hasError":
+            throw postLoadable.contents;
+    }
+    console.log(post);
 
-	const voteHandler = async (post, voteType) => {
-		// http://localhost:3000/api/post/upvote/
-		switch (voteType) {
-			case "up":
-				try {
-					console.log("XDDDDDDDDD", post._id);
-					const response = await axios.post(
-						`http://localhost:3000/api/post/upvote/${post._id}`
-					);
-					console.log(response);
-				} catch (error) {
-					console.log(error);
-				}
-				break;
-			case "down":
-				try {
-					const response = await axios.post(
-						`http://localhost:3000/api/post/downvote/${post._id}`
-					);
-					console.log(response);
-				} catch (error) {
-					console.log(error);
-				}
-				break;
-		}
-		console.log(post, voteType);
-	};
+    const voteHandler = async (post, voteType) => {
+        // http://localhost:3000/api/post/upvote/
+        switch (voteType) {
+            case "up":
+                try {
+                    console.log("XDDDDDDDDD", `${post._id}`);
+                    const response = await axios.post(
+                        `http://localhost:3000/api/post/upvote/${post._id}`
+                    );
+                    console.log(response);
+                } catch (error) {
+                    console.log(error);
+                }
+                break;
+            case "down":
+                try {
+                    const response = await axios.post(
+                        `http://localhost:3000/api/post/downvote/${post._id}`
+                    );
+                    console.log(response);
+                } catch (error) {
+                    console.log(error);
+                }
+                break;
+        }
+        console.log(post, voteType);
+    };
 
-	return (
-		<div>
-			<div className="postpage-wrapper">
-				<div className="postpage-score">
-					<button onClick={() => voteHandler(post, "up")}>&#11014;️</button>
-					<span>{post.votes.upVotes.count - post.votes.downVotes.count}</span>
-					<button onClick={() => voteHandler(post, "down")}>️&#11015;</button>
-				</div>
-				<p className="post-title">{post.title}</p>
-			</div>
-			<p className="post-">
-				{post.author} {moment(parseInt(post.createdAt)).fromNow()}
-			</p>
-			<pre>
-				<p className="post-content">{post.content}</p>
-			</pre>
+    return (
+        <div>
+            <div className="postpage-wrapper">
+                <div className="postpage-score">
+                    <button onClick={() => voteHandler(post, "up")}>
+                        &#11014;️
+                    </button>
+                    <span>
+                        {post.votes.upVotes.count - post.votes.downVotes.count}
+                    </span>
+                    <button onClick={() => voteHandler(post, "down")}>
+                        ️&#11015;
+                    </button>
+                </div>
+                <p className="post-title">{post.title}</p>
+            </div>
+            <p className="post-">
+                {post.author} {moment(parseInt(post.createdAt)).fromNow()}
+            </p>
+            <pre>
+                <p className="post-content">{post.content}</p>
+            </pre>
 
-			<AddComment post={post} />
-			<PostComments post={post} />
-		</div>
-	);
+            <AddComment post={post} />
+            <PostComments post={post} />
+        </div>
+    );
 }
