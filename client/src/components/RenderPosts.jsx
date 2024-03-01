5fbcbccbeb7362cebe6a1d112af7c7fbf52cd254
Dhruv Barot <dhruvbarot892@gmail.com>
2024-02-29 19:07:02 -0500

    sign in component renders appropriately

--- client/src/components/RenderPosts.jsx
+++ client/src/components/RenderPosts.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import moment from "moment";
 import PropTypes from "prop-types";
-import axios from "axios";
+// import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { useSetRecoilState } from "recoil";
-import { refreshPosts } from "../store/atoms";
+// import { useSetRecoilState } from "recoil";
+// import { refreshPosts } from "../store/atoms";
 import { toast } from "react-toastify";
 import { UpVoteLogo } from "./UpVote";
 import { DownVoteLogo } from "./DownVote";
@@ -28,121 +28,128 @@ import { DownVoteLogo } from "./DownVote";
 // }
 
 export function RenderPosts({ post }) {
-	const [score, setScore] = useState(0);
-	const navigate = useNavigate();
-	const setRefreshPosts = useSetRecoilState(refreshPosts);
+    const [score, setScore] = useState(0);
+    const navigate = useNavigate();
+    // const setRefreshPosts = useSetRecoilState(refreshPosts);
 
-	const deleteHandler = async () => {
-		try {
-			await axios.delete(
-				`http://localhost:3000/api/post/deletePost/${post._id}`
-			);
-			setRefreshPosts((prev) => !prev);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+    // const deleteHandler = async () => {
+    // 	try {
+    // 		await axios.delete(
+    // 			`http://localhost:3000/api/post/deletePost/${post._id}`
+    // 		);
+    // 		setRefreshPosts((prev) => !prev);
+    // 	} catch (error) {
+    // 		console.log(error);
+    // 	}
+    // };
 
-	const shareHandler = () => {
-		navigator.clipboard.writeText(`http://localhost:5173/post/${post._id}`);
-		toast.info("Post link copied to clipboard!");
-	};
+    const shareHandler = () => {
+        navigator.clipboard.writeText(`http://localhost:5173/post/${post._id}`);
+        toast.info("Post link copied to clipboard!");
+    };
 
-	return (
-		<>
-			<div className="bg-zinc-900 mb-4 p-4 rounded-lg flex gap-1">
-				<div>
-					<div className="flex flex-col mr-2 flex-initial align-center">
-						<button onClick={() => setScore(score + 1)}>
-							<UpVoteLogo />
-						</button>
-						<span className="text-center">
-							{post.votes.upVotes.count - post.votes.downVotes.count}
-						</span>
-						<button onClick={() => setScore(score - 1)}>
-							<DownVoteLogo />
-						</button>
-					</div>
-				</div>
+    return (
+        <>
+            <div className="bg-zinc-900 mb-4 p-4 rounded-lg flex gap-1">
+                <div>
+                    <div className="flex flex-col mr-2 flex-initial align-center">
+                        <button onClick={() => setScore(score + 1)}>
+                            <UpVoteLogo />
+                        </button>
+                        <span className="text-center">
+                            {post.votes.upVotes.count -
+                                post.votes.downVotes.count}
+                        </span>
+                        <button onClick={() => setScore(score - 1)}>
+                            <DownVoteLogo />
+                        </button>
+                    </div>
+                </div>
 
-				<div>
-					<div>
-						{post.thumbnail !== "self" && <img src={post.thumbnail} alt="" />}
-					</div>
-					<div>
-						<div>
-							<a
-								className="text-xl font-bold cursor-pointer hover:underline"
-								onClick={() => navigate(`/post/${post._id}`)}
-							>
-								<span>{post.title}</span>
-							</a>
-						</div>
-					</div>
-					<div>
-						<span className="text-justify line-clamp-2 mt-2">
-							{post.content}
-						</span>
-					</div>
-					<div className="mt-2 text-gray-300 text-sm">
-						Posted {moment(parseInt(post.createdAt)).fromNow()} by{" "}
-						<span className="font-semibold text-gray-100">{post.author}</span>
-					</div>
-					<div className="mt-2 text-gray-300 flex space-x-2 text-sm ">
-						<a href={post.url}>
-							{post.comments ? `${post.comments.length} comments` : "comment"}
-						</a>
-						<a
-							className="cursor-pointer hover:underline"
-							onClick={shareHandler}
-						>
-							share
-						</a>
-						<a className="cursor-pointer hover:underline">save</a>
-						<a className="cursor-pointer hover:underline">hide</a>
-						<a className="cursor-pointer hover:underline">report</a>
-						<a className="cursor-pointer hover:underline">delete</a>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+                <div>
+                    <div>
+                        {post.thumbnail !== "self" && (
+                            <img src={post.thumbnail} alt="" />
+                        )}
+                    </div>
+                    <div>
+                        <div>
+                            <a
+                                className="text-xl font-bold cursor-pointer hover:underline"
+                                onClick={() => navigate(`/post/${post._id}`)}
+                            >
+                                <span>{post.title}</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div>
+                        <span className="text-justify line-clamp-2 mt-2">
+                            {post.content}
+                        </span>
+                    </div>
+                    <div className="mt-2 text-gray-300 text-sm">
+                        Posted {moment(parseInt(post.createdAt)).fromNow()} by{" "}
+                        <span className="font-semibold text-gray-100">
+                            {post.author}
+                        </span>
+                    </div>
+                    <div className="mt-2 text-gray-300 flex space-x-2 text-sm ">
+                        <a href={post.url}>
+                            {post.comments
+                                ? `${post.comments.length} comments`
+                                : "comment"}
+                        </a>
+                        <a
+                            className="cursor-pointer hover:underline"
+                            onClick={shareHandler}
+                        >
+                            share
+                        </a>
+                        <a className="cursor-pointer hover:underline">save</a>
+                        <a className="cursor-pointer hover:underline">hide</a>
+                        <a className="cursor-pointer hover:underline">report</a>
+                        <a className="cursor-pointer hover:underline">delete</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 const CommentPropTypes = {
-	votes: PropTypes.shape({
-		upVotes: PropTypes.shape({
-			count: PropTypes.number,
-			users: PropTypes.array,
-		}),
-		downVotes: PropTypes.shape({
-			count: PropTypes.number,
-			users: PropTypes.array,
-		}),
-	}),
-	content: PropTypes.string,
-	createdAt: PropTypes.string,
-	author: PropTypes.string,
-	_id: PropTypes.string,
+    votes: PropTypes.shape({
+        upVotes: PropTypes.shape({
+            count: PropTypes.number,
+            users: PropTypes.array,
+        }),
+        downVotes: PropTypes.shape({
+            count: PropTypes.number,
+            users: PropTypes.array,
+        }),
+    }),
+    content: PropTypes.string,
+    createdAt: PropTypes.string,
+    author: PropTypes.string,
+    _id: PropTypes.string,
 };
 
 const PostPropTypes = {
-	_id: PropTypes.string.isRequired,
-	title: PropTypes.string.isRequired,
-	content: PropTypes.string,
-	author: PropTypes.string.isRequired,
-	createdAt: PropTypes.string.isRequired,
-	comments: PropTypes.arrayOf(PropTypes.shape(CommentPropTypes)),
-	votes: PropTypes.shape({
-		upVotes: PropTypes.shape({
-			count: PropTypes.number,
-			users: PropTypes.array,
-		}),
-		downVotes: PropTypes.shape({
-			count: PropTypes.number,
-			users: PropTypes.array,
-		}),
-	}),
+    _id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string,
+    author: PropTypes.string.isRequired,
+    createdAt: PropTypes.string.isRequired,
+    comments: PropTypes.arrayOf(PropTypes.shape(CommentPropTypes)),
+    votes: PropTypes.shape({
+        upVotes: PropTypes.shape({
+            count: PropTypes.number,
+            users: PropTypes.array,
+        }),
+        downVotes: PropTypes.shape({
+            count: PropTypes.number,
+            users: PropTypes.array,
+        }),
+    }),
 };
 
 // Posts.propTypes = {
@@ -150,5 +157,5 @@ const PostPropTypes = {
 // };
 
 RenderPosts.propTypes = {
-	post: PropTypes.shape(PostPropTypes),
+    post: PropTypes.shape(PostPropTypes),
 };
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -11,101 +11,108 @@ import { postAtom } from "../store/atoms";
 import { SignInComponent } from "../components/SignInComponent";
 
 export function Home() {
-	const refreshPostsAtom = useRecoilRefresher_UNSTABLE(fetchPost);
-	const refresh = useRecoilValue(refreshPosts);
-	const navigate = useNavigate();
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const [posts, setPosts] = useState([]);
-	// const [posts, setPosts] = useState([]);
-	// const [userId, setUserId] = useState("");
+    const refreshPostsAtom = useRecoilRefresher_UNSTABLE(fetchPost);
+    const refresh = useRecoilValue(refreshPosts);
+    const navigate = useNavigate();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [posts, setPosts] = useState([]);
+    // const [posts, setPosts] = useState([]);
+    // const [userId, setUserId] = useState("");
+    const isLoggedInHandler = () => {
+        setIsLoggedIn(true);
+    };
+    const fetchSessionData = async () => {
+        try {
+            const response = await axios.get(
+                "http://localhost:3000/api/user/session",
+                {
+                    withCredentials: true,
+                }
+            );
+            if (response.status === 200) {
+                setIsLoggedIn(true);
+                // setUserId(response.data.userId);
+            }
+        } catch (error) {
+            console.log("User is not authenticated");
+        }
+    };
 
-	const fetchSessionData = async () => {
-		try {
-			const response = await axios.get(
-				"http://localhost:3000/api/user/session",
-				{
-					withCredentials: true,
-				}
-			);
-			if (response.status === 200) {
-				setIsLoggedIn(true);
-				// setUserId(response.data.userId);
-			}
-		} catch (error) {
-			console.log("User is not authenticated");
-		}
-	};
+    const logoutHandler = async () => {
+        try {
+            await axios.get("http://localhost:3000/api/user/signout", {
+                withCredentials: true,
+            });
+            setIsLoggedIn(false);
+            toast.success("Successfully logged out!");
+        } catch (error) {
+            console.error(error);
+            toast.error("Error occurred while logging out.");
+        }
+    };
 
-	const logoutHandler = async () => {
-		try {
-			await axios.get("http://localhost:3000/api/user/signout", {
-				withCredentials: true,
-			});
-			setIsLoggedIn(false);
-			toast.success("Successfully logged out!");
-		} catch (error) {
-			console.error(error);
-			toast.error("Error occurred while logging out.");
-		}
-	};
+    const postsLoadable = useRecoilValueLoadable(postAtom);
 
-	const postsLoadable = useRecoilValueLoadable(postAtom);
+    useEffect(() => {
+        refreshPostsAtom();
+        fetchSessionData();
+    }, [refresh, refreshPostsAtom, isLoggedIn]);
 
-	useEffect(() => {
-		refreshPostsAtom();
-		fetchSessionData();
-	}, [refresh, refreshPostsAtom, isLoggedIn]);
+    useEffect(() => {
+        if (postsLoadable.state === "hasValue") {
+            setPosts(postsLoadable.contents);
+        }
+    }, [postsLoadable]);
 
-	useEffect(() => {
-		if (postsLoadable.state === "hasValue") {
-			setPosts(postsLoadable.contents);
-		}
-	}, [postsLoadable]);
+    const setPostsHandler = (newPost) => {
+        setPosts((prev) => [...prev, newPost]);
+    };
 
-	const setPostsHandler = (newPost) => {
-		setPosts((prev) => [...prev, newPost]);
-	};
+    switch (postsLoadable.state) {
+        case "hasValue":
+            return (
+                <div className="max-w-4xl mx-auto text-wrap text-gray-200">
+                    <div className="bg-zinc-900 p-5 self-start my-5 rounded flex flex-col items-center justify-center">
+                        {isLoggedIn ? (
+                            <>
+                                <button
+                                    className="bg-zinc-800"
+                                    onClick={logoutHandler}
+                                >
+                                    Log out
+                                </button>
+                                <CreatePost setPostsHandler={setPostsHandler} />
+                            </>
+                        ) : (
+                            <SignInComponent
+                                isLoggedInHandler={isLoggedInHandler}
+                            />
+                        )}
+                        {!isLoggedIn && (
+                            <div className="mt-5 flex flex-col">
+                                <p>Dont have an account? Sign up below!</p>
+                                <button
+                                    className="bg-zinc-800 mt-2"
+                                    onClick={() => {
+                                        navigate("/signUpIn");
+                                    }}
+                                >
+                                    Sign Up
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                    {posts.map((post) => (
+                        <RenderPosts key={post._id} post={post} />
+                    ))}
+                </div>
+            );
 
-	switch (postsLoadable.state) {
-		case "hasValue":
-			return (
-				<div className="max-w-4xl mx-auto text-wrap text-gray-200">
-					<div className="bg-zinc-900 p-5 self-start my-5 rounded flex flex-col items-center justify-center">
-						{isLoggedIn ? (
-							<>
-								<button className="bg-zinc-800" onClick={logoutHandler}>
-									Log out
-								</button>
-								<CreatePost setPostsHandler={setPostsHandler} />
-							</>
-						) : (
-							<SignInComponent />
-						)}
-						{!isLoggedIn && (
-							<div className="mt-5 flex flex-col">
-								<p>Dont have an account? Sign up below!</p>
-								<button
-									className="bg-zinc-800 mt-2"
-									onClick={() => {
-										navigate("/signUpIn");
-									}}
-								>
-									Sign Up
-								</button>
-							</div>
-						)}
-					</div>
-					{posts.map((post) => (
-						<RenderPosts key={post._id} post={post} />
-					))}
-				</div>
-			);
-
-		case "loading":
-			return <div>Loading...</div>;
-		case "hasError":
-			return <div>Error: {postsLoadable.contents.message}</div>;
-		default:
-			return null;
-	}
+        case "loading":
+            return <div>Loading...</div>;
+        case "hasError":
+            return <div>Error: {postsLoadable.contents.message}</div>;
+        default:
+            return null;
+    }
 }
