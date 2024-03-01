63608f566094f2c2ba3092e8fb7a8f980c3a84a7
Darshan Vaishya <DarshanVaishya3@gmail.com>
2024-03-01 14:06:12 -0500

    Updated notification style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,19 @@ function App() {
 					<Route path="/signupin" element={<SignUpIn />} />
 				</Routes>
 			</BrowserRouter>
-			<ToastContainer />
+			<ToastContainer
+				position="bottom-center"
+				autoClose={5000}
+				hideProgressBar
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="dark"
+				transition:Slide
+			/>
 		</RecoilRoot>
 	);
 }
