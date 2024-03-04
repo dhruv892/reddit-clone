549f6a9501b3d03c404b2091678ae1f66271b55f
Dhruv Barot <dhruvbarot892@gmail.com>
2024-03-04 11:16:38 -0500

    yet to resolvr infinitr loop in HomePage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,46 @@
 import { Home } from "./pages/home";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { PostPage } from "./pages/PostPage";
 import { SignUpIn } from "./pages/SignUpIn";
 import { ToastContainer } from "react-toastify";
+import { ErrorBoundary } from "react-error-boundary";
 import "react-toastify/dist/ReactToastify.css";
 import { NavbarComponent } from "./components/NavbarComponent";
 
 function App() {
-	return (
-		<RecoilRoot>
-			<BrowserRouter>
-				<NavbarComponent />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/post/:id" element={<PostPage />} />
-					<Route path="/signupin" element={<SignUpIn />} />
-				</Routes>
-			</BrowserRouter>
-			<ToastContainer />
-		</RecoilRoot>
-	);
+    // return (
+    //     <RecoilRoot>
+    //         <BrowserRouter>
+    //             <NavbarComponent />
+    //             <Routes>
+    //                 <Route path="/" element={<Home />} />
+    //                 <Route path="/post/:id" element={<PostPage />} />
+    //                 <Route path="/signupin" element={<SignUpIn />} />
+    //             </Routes>
+    //         </BrowserRouter>
+    //         <ToastContainer />
+    //     </RecoilRoot>
+    // );
+    return (
+        <RecoilRoot>
+            <ErrorBoundary>
+                <React.Suspense fallback={<div>Loading...</div>}>
+                    <BrowserRouter>
+                        <NavbarComponent />
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/post/:id" element={<PostPage />} />
+                            <Route path="/signupin" element={<SignUpIn />} />
+                        </Routes>
+                    </BrowserRouter>
+                </React.Suspense>
+            </ErrorBoundary>
+
+            <ToastContainer />
+        </RecoilRoot>
+    );
 }
 
 export default App;
