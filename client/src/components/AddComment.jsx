8ce22ecb09baad57b8399e68cf28ff2b2ce30696
Darshan Vaishya <DarshanVaishya3@gmail.com>
2024-03-16 14:16:48 -0400

    Add comment is hidden after submission

--- client/src/components/AddComment.jsx
+++ client/src/components/AddComment.jsx
@@ -2,77 +2,78 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
 
-export default function AddComment({ id, setCommentsHandler }) {
-    const [content, setContent] = useState("");
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const fetchSessionData = async () => {
-        try {
-            const response = await axios.get(
-                "http://localhost:3000/api/user/session",
-                {
-                    withCredentials: true,
-                }
-            );
-            if (response.status === 200) {
-                setIsLoggedIn(true);
-            }
-        } catch (error) {
-            console.log("User is not authenticated");
-        }
-    };
+export default function AddComment({ id, setCommentsHandler, setDoReply }) {
+	const [content, setContent] = useState("");
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const fetchSessionData = async () => {
+		try {
+			const response = await axios.get(
+				"http://localhost:3000/api/user/session",
+				{
+					withCredentials: true,
+				}
+			);
+			if (response.status === 200) {
+				setIsLoggedIn(true);
+			}
+		} catch (error) {
+			console.log("User is not authenticated");
+		}
+	};
 
-    const submitHandler = async (e) => {
-        e.preventDefault();
-        try {
-            if (!content) {
-                return;
-            }
-            const response = await axios.post(
-                `http://localhost:3000/api/post/addComment/${id}`,
-                {
-                    content,
-                    createdAt: String(Date.now()),
-                },
-                {
-                    withCredentials: true,
-                }
-            );
-            setCommentsHandler(response.data.comment);
-            console.log(response);
-            setContent("");
-            // doReplyHandler();
-        } catch (error) {
-            console.log(error);
-        }
-    };
+	const submitHandler = async (e) => {
+		e.preventDefault();
+		try {
+			if (!content) {
+				return;
+			}
+			const response = await axios.post(
+				`http://localhost:3000/api/post/addComment/${id}`,
+				{
+					content,
+					createdAt: String(Date.now()),
+				},
+				{
+					withCredentials: true,
+				}
+			);
+			setCommentsHandler(response.data.comment);
+			console.log(response);
+			setContent("");
+			setDoReply(false);
+			// doReplyHandler();
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-    useEffect(() => {
-        fetchSessionData();
-    }, []);
+	useEffect(() => {
+		fetchSessionData();
+	}, []);
 
-    if (!isLoggedIn) {
-        return <h4>Log in to comment</h4>;
-    }
+	if (!isLoggedIn) {
+		return <h4>Log in to comment</h4>;
+	}
 
-    return (
-        <div className="bg-zinc-900 rounded-lg p-4 mt-5">
-            <form className="flex gap-5" onSubmit={submitHandler}>
-                <input
-                    className="bg-zinc-800 placeholder:text-zinc-500 flex-grow p-2 rounded-lg"
-                    type="text"
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                    placeholder="Comment"
-                />
-                <button
-                    className="py-2 px-4 flex-initial rounded-lg bg-zinc-700"
-                    onClick={submitHandler}
-                >
-                    Add comment
-                </button>
-            </form>
-        </div>
-    );
+	return (
+		<div className="bg-zinc-900 rounded-lg py-2 mt-2">
+			<form className="flex gap-5" onSubmit={submitHandler}>
+				<input
+					className="bg-zinc-800 placeholder:text-zinc-500 flex-grow p-2 rounded-lg"
+					type="text"
+					value={content}
+					onChange={(e) => setContent(e.target.value)}
+					placeholder="Comment"
+				/>
+				<button
+					className="py-2 px-4 flex-initial rounded-lg bg-zinc-700"
+					onClick={submitHandler}
+				>
+					Add comment
+				</button>
+			</form>
+		</div>
+	);
 }
 // const CommentPropTypes = {
 //     votes: PropTypes.shape({
@@ -111,6 +112,6 @@ export default function AddComment({ id, setCommentsHandler }) {
 // };
 
 AddComment.propTypes = {
-    id: PropTypes.string.isRequired,
-    setCommentsHandler: PropTypes.func,
+	id: PropTypes.string.isRequired,
+	setCommentsHandler: PropTypes.func,
 };
