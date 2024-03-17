875b3eef61254bd48ef1e40df1a53cc8736b7278
Dhruv Barot <dhruvbarot892@gmail.com>
2024-03-17 03:48:10 -0400

    minor fixes after merge

--- client/src/components/AddComment.jsx
+++ client/src/components/AddComment.jsx
@@ -3,77 +3,77 @@ import axios from "axios";
 import PropTypes from "prop-types";
 
 export default function AddComment({ id, setCommentsHandler, setDoReply }) {
-	const [content, setContent] = useState("");
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const fetchSessionData = async () => {
-		try {
-			const response = await axios.get(
-				"http://localhost:3000/api/user/session",
-				{
-					withCredentials: true,
-				}
-			);
-			if (response.status === 200) {
-				setIsLoggedIn(true);
-			}
-		} catch (error) {
-			console.log("User is not authenticated");
-		}
-	};
+    const [content, setContent] = useState("");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const fetchSessionData = async () => {
+        try {
+            const response = await axios.get(
+                "http://localhost:3000/api/user/session",
+                {
+                    withCredentials: true,
+                }
+            );
+            if (response.status === 200) {
+                setIsLoggedIn(true);
+            }
+        } catch (error) {
+            console.log("User is not authenticated");
+        }
+    };
 
-	const submitHandler = async (e) => {
-		e.preventDefault();
-		try {
-			if (!content) {
-				return;
-			}
-			const response = await axios.post(
-				`http://localhost:3000/api/post/addComment/${id}`,
-				{
-					content,
-					createdAt: String(Date.now()),
-				},
-				{
-					withCredentials: true,
-				}
-			);
-			setCommentsHandler(response.data.comment);
-			console.log(response);
-			setContent("");
-			setDoReply(false);
-			// doReplyHandler();
-		} catch (error) {
-			console.log(error);
-		}
-	};
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        try {
+            if (!content) {
+                return;
+            }
+            const response = await axios.post(
+                `http://localhost:3000/api/post/addComment/${id}`,
+                {
+                    content,
+                    createdAt: String(Date.now()),
+                },
+                {
+                    withCredentials: true,
+                }
+            );
+            setCommentsHandler(response.data.comment);
+            console.log(response);
+            setContent("");
+            setDoReply();
+            // doReplyHandler();
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-	useEffect(() => {
-		fetchSessionData();
-	}, []);
+    useEffect(() => {
+        fetchSessionData();
+    }, []);
 
-	if (!isLoggedIn) {
-		return <h4>Log in to comment</h4>;
-	}
+    if (!isLoggedIn) {
+        return <h4>Log in to comment</h4>;
+    }
 
-	return (
-		<div className="bg-zinc-900 rounded-lg py-2 mt-2">
-			<form className="flex gap-5" onSubmit={submitHandler}>
-				<input
-					className="bg-zinc-800 placeholder:text-zinc-500 flex-grow p-2 rounded-lg"
-					type="text"
-					value={content}
-					onChange={(e) => setContent(e.target.value)}
-					placeholder="Comment"
-				/>
-				<button
-					className="py-2 px-4 flex-initial rounded-lg bg-zinc-700"
-					onClick={submitHandler}
-				>
-					Add comment
-				</button>
-			</form>
-		</div>
-	);
+    return (
+        <div className="bg-zinc-900 rounded-lg py-2 mt-2">
+            <form className="flex gap-5" onSubmit={submitHandler}>
+                <input
+                    className="bg-zinc-800 placeholder:text-zinc-500 flex-grow p-2 rounded-lg"
+                    type="text"
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    placeholder="Comment"
+                />
+                <button
+                    className="py-2 px-4 flex-initial rounded-lg bg-zinc-700"
+                    onClick={submitHandler}
+                >
+                    Add comment
+                </button>
+            </form>
+        </div>
+    );
 }
 // const CommentPropTypes = {
 //     votes: PropTypes.shape({
@@ -112,6 +112,7 @@ export default function AddComment({ id, setCommentsHandler, setDoReply }) {
 // };
 
 AddComment.propTypes = {
-	id: PropTypes.string.isRequired,
-	setCommentsHandler: PropTypes.func,
+    id: PropTypes.string.isRequired,
+    setCommentsHandler: PropTypes.func,
+    setDoReply: PropTypes.func,
 };
--- client/src/components/PostComments.jsx
+++ client/src/components/PostComments.jsx
@@ -6,108 +6,108 @@ import { VotingComponent } from "./VotingComponent";
 import AddComment from "./AddComment";
 
 export function PostComments({ comment, userId, setCountHandler }) {
-	axios.defaults.withCredentials = true;
+    axios.defaults.withCredentials = true;
 
-	// const setRefreshPosts = useSetRecoilState(refreshPosts);
-	const [doReply, setDoReply] = useState(false);
-	// const [replyContent, setReplyContent] = useState("");
-	const [replies, setReplies] = useState([]);
+    // const setRefreshPosts = useSetRecoilState(refreshPosts);
+    const [doReply, setDoReply] = useState(false);
+    // const [replyContent, setReplyContent] = useState("");
+    const [replies, setReplies] = useState([]);
 
-	useEffect(() => {
-		// console.log(comment._id);
-		if (!comment) return;
-		if (comment.replies && comment.replies.length > 0)
-			setReplies(comment.replies);
-		// setUpVoteUsers();
-	}, [comment]);
+    useEffect(() => {
+        // console.log(comment._id);
+        if (!comment) return;
+        if (comment.replies && comment.replies.length > 0)
+            setReplies(comment.replies);
+        // setUpVoteUsers();
+    }, [comment]);
 
-	const setRepliesHandler = (newComment) => {
-		setReplies((prev) => [...prev, newComment]);
-		setCountHandler();
-	};
+    const setRepliesHandler = (newComment) => {
+        setReplies((prev) => [...prev, newComment]);
+        setCountHandler();
+    };
 
-	// const doReplyHandler = () => {
-	//     setDoReply((prev) => !prev);
-	// };
+    const setDoReplyHandler = () => {
+        setDoReply((prev) => !prev);
+    };
 
-	return (
-		<div className="flex gap-5 m-2 p-2">
-			<div className="flex flex-col flex-initial align-center">
-				<VotingComponent
-					votes={comment.votes}
-					userId={userId}
-					type={"comment"}
-					itemId={comment._id}
-				/>
-			</div>
-			<div className="flex-grow">
-				<p className="mb-1">
-					<span className="font-bold">{comment.author} </span>
-					<span className="text-zinc-500 ml-1">
-						{moment(parseInt(comment.createdAt)).fromNow()}{" "}
-					</span>
-				</p>
+    return (
+        <div className="flex gap-5 m-2 p-2">
+            <div className="flex flex-col flex-initial align-center">
+                <VotingComponent
+                    votes={comment.votes}
+                    userId={userId}
+                    type={"comment"}
+                    itemId={comment._id}
+                />
+            </div>
+            <div className="flex-grow">
+                <p className="mb-1">
+                    <span className="font-bold">{comment.author} </span>
+                    <span className="text-zinc-500 ml-1">
+                        {moment(parseInt(comment.createdAt)).fromNow()}{" "}
+                    </span>
+                </p>
 
-				<p className="mb-1">{comment.content}</p>
-				{!doReply ? (
-					<span
-						style={{
-							color: "gray",
-						}}
-						onClick={() => {
-							setDoReply(true);
-						}}
-					>
-						reply
-					</span>
-				) : (
-					<div>
-						<AddComment
-							id={comment._id}
-							setCommentsHandler={setRepliesHandler}
-							// doReplyHandler={doReplyHandler}
-							setDoReply={setDoReply}
-						/>
-					</div>
-				)}
-				<div className="border-l border-l-zinc-50">
-					{replies.length > 0
-						? replies.map((reply) => (
-								<PostComments
-									key={reply._id}
-									comment={reply}
-									userId={userId}
-									setCountHandler={setCountHandler}
-								/>
-						  ))
-						: null}
-				</div>
-			</div>
-		</div>
-	);
+                <p className="mb-1">{comment.content}</p>
+                {!doReply ? (
+                    <span
+                        style={{
+                            color: "gray",
+                        }}
+                        onClick={() => {
+                            setDoReply(true);
+                        }}
+                    >
+                        reply
+                    </span>
+                ) : (
+                    <div>
+                        <AddComment
+                            id={comment._id}
+                            setCommentsHandler={setRepliesHandler}
+                            // doReplyHandler={doReplyHandler}
+                            setDoReply={setDoReplyHandler}
+                        />
+                    </div>
+                )}
+                <div className="border-l border-l-zinc-50">
+                    {replies.length > 0
+                        ? replies.map((reply) => (
+                              <PostComments
+                                  key={reply._id}
+                                  comment={reply}
+                                  userId={userId}
+                                  setCountHandler={setCountHandler}
+                              />
+                          ))
+                        : null}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 const CommentPropTypes = () => ({
-	votes: PropTypes.shape({
-		upVotes: PropTypes.shape({
-			count: PropTypes.number,
-			users: PropTypes.array,
-		}),
-		downVotes: PropTypes.shape({
-			count: PropTypes.number,
-			users: PropTypes.array,
-		}),
-	}),
-	content: PropTypes.string,
-	createdAt: PropTypes.string,
-	author: PropTypes.string,
-	_id: PropTypes.string,
+    votes: PropTypes.shape({
+        upVotes: PropTypes.shape({
+            count: PropTypes.number,
+            users: PropTypes.array,
+        }),
+        downVotes: PropTypes.shape({
+            count: PropTypes.number,
+            users: PropTypes.array,
+        }),
+    }),
+    content: PropTypes.string,
+    createdAt: PropTypes.string,
+    author: PropTypes.string,
+    _id: PropTypes.string,
 
-	replies: PropTypes.arrayOf(PropTypes.shape(CommentPropTypes)),
+    replies: PropTypes.arrayOf(PropTypes.shape(CommentPropTypes)),
 });
 
 PostComments.propTypes = {
-	comment: PropTypes.shape(CommentPropTypes),
-	userId: PropTypes?.string,
-	setCountHandler: PropTypes.func,
+    comment: PropTypes.shape(CommentPropTypes),
+    userId: PropTypes?.string,
+    setCountHandler: PropTypes.func,
 };
