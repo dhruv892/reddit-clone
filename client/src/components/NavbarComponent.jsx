244eadfca4565c345bbd9b58b91143ab06c094f1
Darshan Vaishya <DarshanVaishya3@gmail.com>
2024-03-25 19:15:37 -0400

    Removed unnecessary parts from navbar

--- client/src/components/NavbarComponent.jsx
+++ client/src/components/NavbarComponent.jsx
@@ -67,51 +67,17 @@ export function NavbarComponent() {
 					Reddit
 				</span>
 			</div>
-			<div className="block lg:hidden">
-				<button className="flex items-center px-3 py-2 border rounded text-zinc-200 border-teal-400 hover:text-white hover:border-white">
-					<svg
-						className="fill-current h-3 w-3"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
+			{user && (
+				<div>
+					<span className="text-gray-200 text-xl">{user.username}</span>
+					<button
+						className="inline-block ml-5 text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-500 hover:bg-white mt-4 lg:mt-0"
+						onClick={logoutHandler}
 					>
-						<title>Menu</title>
-						<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-					</svg>
-				</button>
-			</div>
-			<div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-				<div className="text-sm lg:flex-grow">
-					{/* <a
-						href="#responsive-header"
-						className="block mt-4 lg:inline-block lg:mt-0 text-zinc-200 hover:text-white mr-4"
-					>
-						Docs
-					</a>
-					<a
-						href="#responsive-header"
-						className="block mt-4 lg:inline-block lg:mt-0 text-zinc-200 hover:text-white mr-4"
-					>
-						Examples
-					</a>
-					<a
-						href="#responsive-header"
-						className="block mt-4 lg:inline-block lg:mt-0 text-zinc-200 hover:text-white"
-					>
-						Blog
-					</a> */}
+						Log out
+					</button>
 				</div>
-				{user && (
-					<div>
-						<span className="text-gray-200 text-xl">{user.username}</span>
-						<button
-							className="inline-block ml-5 text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-500 hover:bg-white mt-4 lg:mt-0"
-							onClick={logoutHandler}
-						>
-							Log out
-						</button>
-					</div>
-				)}
-			</div>
+			)}
 		</nav>
 	);
 }
