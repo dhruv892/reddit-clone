84d4bfa3e25ab3d846fea2e577dda809ab316d2a
Dhruv Barot <dhruvbarot892@gmail.com>
2024-04-11 16:02:01 -0400

    "trust proxy"

--- server/server.js
+++ server/server.js
@@ -1,31 +1,32 @@
- const express = require("express");
- const cors = require("cors");
- const app = express();
- const rootRouter = require("./routes/index");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const rootRouter = require("./routes/index");
 
- const corsOptions = {
-		origin: function (origin, callback) {
-			const allowedOrigins = [
-				"http://localhost:5173",
-				"http://localhost:5174",
-				"http://another-origin.com",
-				"https://reddit-clone.darshanv.dev",
-			];
-			if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-				callback(null, true);
-			} else {
-				callback(new Error("Not allowed by CORS"));
-			}
-		},
-		credentials: true,
- };
+const corsOptions = {
+    origin: function (origin, callback) {
+        const allowedOrigins = [
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://another-origin.com",
+            "https://reddit-clone.darshanv.dev",
+        ];
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+};
+app.set("trust proxy", 1);
 
- app.use(cors(corsOptions));
- app.use(express.json());
- app.use("/api", rootRouter);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use("/api", rootRouter);
 
- // Use the PORT environment variable provided by Heroku, or default to 3000
- const port = process.env.PORT || 3000;
- app.listen(port, () => {
-		console.log(`Server running on port ${port}`);
- });
+// Use the PORT environment variable provided by Heroku, or default to 3000
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
