42ce89459058eb9b7b350d0c40826ce8af6e3065
Darshan Vaishya <DarshanVaishya3@gmail.com>
2024-04-11 16:37:23 -0400

    Fixed cors issue

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,29 +10,29 @@ require("dotenv").config();
 const router = express.Router();
 
 mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("MongoDB connected"))
-    .catch((err) => console.error("MongoDB connection error:", err));
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("MongoDB connected"))
+	.catch((err) => console.error("MongoDB connection error:", err));
 
 // Initialize session middleware
 router.use(
-    session({
-        name: "sessionForRedditClone",
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        sameSite: true,
-        saveUninitialized: false,
-        store: MongoStore.create({ mongoUrl: process.env.MONGO_URL }),
-        cookie: {
-            secure: true,
-            httpOnly: true,
-            sameSite: "secure",
-            maxAge: 1000 * 60 * 60 * 24,
-        },
-    })
+	session({
+		name: "sessionForRedditClone",
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		sameSite: true,
+		saveUninitialized: false,
+		store: MongoStore.create({ mongoUrl: process.env.MONGO_URL }),
+		cookie: {
+			secure: true,
+			httpOnly: true,
+			sameSite: "lax",
+			maxAge: 1000 * 60 * 60 * 24,
+		},
+	})
 );
 
 router.use("/user", userRouter);
