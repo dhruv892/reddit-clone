ea761633ed4e805ba6f903b924ef9a7028c317d4
Darshan Vaishya <DarshanVaishya3@gmail.com>
2024-04-11 14:53:32 -0400

    Prepare for heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,28 +1,30 @@
-const express = require("express");
-const cors = require("cors");
-// const postsController = require("./controller/postsController");
+ const express = require("express");
+ const cors = require("cors");
+ const app = express();
+ const rootRouter = require("./routes/index");
 
-const app = express();
-const rootRouter = require("./routes/index");
+ const corsOptions = {
+		origin: function (origin, callback) {
+			const allowedOrigins = [
+				"http://localhost:5173",
+				"http://localhost:5174",
+				"http://another-origin.com",
+			];
+			if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+				callback(null, true);
+			} else {
+				callback(new Error("Not allowed by CORS"));
+			}
+		},
+		credentials: true,
+ };
 
-const corsOptions = {
-    origin: function (origin, callback) {
-        const allowedOrigins = [
-            "http://localhost:5173",
-            "http://localhost:5174",
-            "http://another-origin.com",
-        ];
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true,
-};
+ app.use(cors(corsOptions));
+ app.use(express.json());
+ app.use("/api", rootRouter);
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use("/api", rootRouter);
-
-app.listen(3000);
+ // Use the PORT environment variable provided by Heroku, or default to 3000
+ const port = process.env.PORT || 3000;
+ app.listen(port, () => {
+		console.log(`Server running on port ${port}`);
+ });
