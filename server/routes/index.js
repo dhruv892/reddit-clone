a06c4e80b04332ea2c6ccf3ccb761f552d1a5628
Dhruv Barot <dhruvbarot892@gmail.com>
2024-04-14 13:46:44 -0400

    removed redundant bits

--- server/routes/index.js
+++ server/routes/index.js
@@ -17,25 +17,18 @@ mongoose
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error("MongoDB connection error:", err));
 
-console.log(
-    "####################################################################"
-);
-console.log(process.env);
 // Initialize session middleware
 router.use(
     session({
         name: "sessionForRedditClone",
         secret: process.env.SESSION_SECRET,
         resave: false,
-        // sameSite: true,
         saveUninitialized: false,
         store: MongoStore.create({ mongoUrl: process.env.MONGO_URL }),
         cookie: {
             sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // must be 'none' to enable cross-site delivery
             secure: process.env.NODE_ENV === "production", // must be true if sameSite='none'
-            // secure: true,
             httpOnly: true,
-            // sameSite: "secure",
             maxAge: 1000 * 60 * 60 * 24,
         },
     })
