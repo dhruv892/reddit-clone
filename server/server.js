83b23caf154ed9d5b090292ea14210cea6420128
Dhruv Barot <dhruvbarot892@gmail.com>
2025-09-26 23:44:44 -0400

    minor

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const corsOptions = {
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:3000",
+      process.env.PRODUCTION_URL,
     ];
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
@@ -26,15 +27,15 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use;
 const path = require("path");
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-}
+const __dirname = path.resolve();
+
 app.use("/api", rootRouter);
 
 // Serve the frontend for any other routes not handled by the API
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
   });
 }
 // Use the PORT environment variable provided by Heroku, or default to 3000
